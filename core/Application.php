46845f9e1c119f28b9f9af80c0b9d3e0c7d46aec
cheyawanigs <chethiya.wanigarathne@gmail.com>
2024-03-06 19:13:25 +0530

    developing database connection

--- core/Application.php
+++ core/Application.php
@@ -21,15 +21,15 @@ class Application
     {
         $this->controller = $controller;
     }
-    public function __construct($rootPath)
+    public function __construct($rootPath, array $config)
     {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
-        $this->response = new Response();
         $this->request = new Request();
+        $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
 
-        $this->db = new Database();
+        $this->db = new Database($config['db']);
 
     }
     public function run()
--- core/Database.php
+++ core/Database.php
@@ -15,8 +15,7 @@ class Database
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
-    {
+
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    }
 }
